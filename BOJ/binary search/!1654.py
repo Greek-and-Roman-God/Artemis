e245a720e78ce35dc0705b37a5f75c0afc956b97
zwooo96 <dlwldn385@naver.com>
2020-11-17 02:44:18 +0000

    BOJ 이분탐색

---
+++ BOJ/binary search/!1654.py
@@ -0,0 +1,25 @@
+#메모리초과로 실패
+
+from sys import stdin
+import copy
+
+k, n = map(int, stdin.readline().split())
+K = []
+for _ in range(k) :
+  K.append(int(stdin.readline()))
+N = n   #재귀호출시 n값을 고정시키기 위해 사용하는 변수
+
+def cutting_lan(N, n, K) :
+  temp = copy.deepcopy(K)   #K값을 고정시키기 위해 깊은 복사
+  cnt = 0
+  lan = temp[0] // n
+  for i in range(k) :
+    cnt += temp[i] // lan
+    temp[i] %= lan
+  if cnt > N :
+    cutting_lan(N, n-1, K)
+  else :
+    print(lan)
+    return
+
+cutting_lan(N, n, K)
