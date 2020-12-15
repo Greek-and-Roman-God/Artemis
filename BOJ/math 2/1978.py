0e355a5fa610387db308b394ef8f64c999285d3d
Jiwoo Yi <dlwldn385@naver.com>
2020-12-15 07:14:54 +0000

    BOJ 수학2

---
+++ BOJ/math 2/1978.py
@@ -0,0 +1,13 @@
+N = int(input())
+n_list = list(map(int, input().split()))
+
+cnt = 0
+for n in n_list :
+  if n != 1 : #1은 소수가 아님
+    for i in range(2, n) :#1과 자기자신을 제외한 수로 나뉘면 X
+      if n % i == 0 :
+        break
+    else :
+      cnt += 1
+
+print(cnt)
