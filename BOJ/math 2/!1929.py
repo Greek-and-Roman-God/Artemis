b9d31d5ae67ee0b31f70c0c0aa30676b2a961730
Jiwoo Yi <dlwldn385@naver.com>
2020-12-17 15:34:35 +0000

    BOJ 수학2 소수 구하기

--- BOJ/math 2/!1929.py
+++ BOJ/math 2/!1929.py
@@ -8,10 +8,13 @@ prime_list = []
 for i in range(M, N+1) :
   prime_list.append(i)
 
+remove_list = []
 for i in range(2, int(math.sqrt(N))+1) :
-  for num in prime_list :
-    if num % i == 0 and num != i :
-      prime_list.remove(num)
+  if i not in remove_list :
+    for num in prime_list :
+      if num % i == 0 and num != i :
+        prime_list.remove(num)
+        remove_list.append(num)
 
 for prime_num in prime_list :
   print(prime_num)
