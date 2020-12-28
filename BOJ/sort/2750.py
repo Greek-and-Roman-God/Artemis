cf5f7d5ddac5d0e5530f37c7c4b78ccccc115675
Jiwoo Yi <dlwldn385@naver.com>
2020-12-28 07:34:40 +0000

    BOJ 정렬 수 정렬하기

--- BOJ/sort/2750.py
+++ BOJ/sort/2750.py
@@ -1,3 +1,4 @@
+#sort 사용O
 N = int(input())
 
 result = []
@@ -5,5 +6,20 @@ for _ in range(N) :
   result.append(int(input()))
 
 result.sort()
+for i in result :
+  print(i)
+
+#sort 사용X
+N = int(input())
+
+result = []
+for _ in range(N) :
+  result.append(int(input()))
+
+for i in range(len(result)-1) :
+  for j in range(i, len(result)) :
+    if result[i] > result [j] :
+      result[i], result[j] = result[j], result[i]
+
 for i in result :
   print(i)
