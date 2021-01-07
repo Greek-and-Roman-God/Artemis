85800e4d6d61faddb86819a166e71732b8680973
Jiwoo Yi <dlwldn385@naver.com>
2021-01-07 16:45:15 +0000

    BOJ 백트래킹 N과 M (1)

--- BOJ/backtracking/15649.py
+++ BOJ/backtracking/15649.py
@@ -1,3 +1,25 @@
+N, M = map(int, input().split())
+
+visited = [False] * N #방문여부
+result = []
+
+def DFS(temp, N, M) :
+  if temp == M :
+    print(" ".join(map(str, result)))
+    return
+  
+  for i in range(len(visited)) :  
+    if not visited[i] : #방문하지 않은 노드라면
+      visited[i] = True
+      result.append(i+1)
+      DFS(temp+1, N, M) #깊이우선탐색
+      visited[i] = False
+      result.pop()
+
+DFS(0, N, M)
+
+
+#permutations 이용한 풀이법
 from itertools import permutations
 
 N, M = map(int, input().split())
