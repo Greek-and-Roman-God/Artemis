8b3a737ed5051849a6b4f839b82807ec57fd1cb4
Jiwoo Yi <dlwldn385@naver.com>
2021-03-19 07:10:29 +0000

    숫자 카드 게임

--- 이코테/greedy/part2_03.py
+++ 이코테/greedy/part2_03.py
@@ -1,3 +1,14 @@
+#210319
+N, M = map(int, input().split())
+cards = [list(map(int, input().split())) for _ in range(N)]
+
+result = 0
+for i in range(N) :
+  result = max(min(cards[i]), result) #각 행의 가장 작은 숫자 중 큰 숫자를 저장
+
+print(result)
+
+#201111
 n, m = map(int, input().split())
 data = [0] * m
 temp = []
