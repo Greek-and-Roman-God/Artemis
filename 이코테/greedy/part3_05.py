86f8c78a4db9d2656cf9145785d2ccbf1b56ec2e
Jiwoo Yi <dlwldn385@naver.com>
2021-04-21 16:37:06 +0000

    볼링공 고르기

--- 이코테/greedy/part3_05.py
+++ 이코테/greedy/part3_05.py
@@ -1,3 +1,16 @@
+#210422
+N, M = map(int, input().split())
+ball = list(map(int, input().split()))
+
+result = 0
+for i in range(N) :
+  for j in range(i+1, N) :
+    if ball[i] != ball[j] and ball[i] <= M and ball[j] <= M : #최대 무게를 넘지 않는 공이고, 같은 무게가 아니라면
+      result += 1
+
+print(result)
+
+#201111
 n, m = map(int, input().split())
 wList = list(map(int, input().split()))
 
