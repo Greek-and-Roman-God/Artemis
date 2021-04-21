fa5ee9be0fc5f8fa71d156a3c76e543fe55c5a24
Jiwoo Yi <dlwldn385@naver.com>
2021-04-21 08:14:33 +0000

    모험가 길드

--- 이코테/greedy/part3_01.py
+++ 이코테/greedy/part3_01.py
@@ -1,3 +1,20 @@
+#210421
+N = int(input())
+guild = list(map(int, input().split()))
+guild.sort() #공포도 순으로 정렬
+
+result = 0 #여행을 떠날 그룹 수
+tmp = 0 #각 그룹의 인원
+for i in guild :
+  fear = i #높은 공포도를 가진 사람이 있으면 공포도를 변경
+  tmp += 1
+  if fear == tmp : #공포도와 인원 수가 일치하면 여행을 떠남
+    result += 1
+    tmp = 0
+
+print(result)
+
+#201111
 n = int(input())
 fearList = list(map(int, input().split()))
 fearList.sort()
