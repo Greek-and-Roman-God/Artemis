a8e4b8d5ede8a199744efdcce53146b6bceebfee
Jiwoo Yi <dlwldn385@naver.com>
2021-04-21 13:56:41 +0000

    문자열 뒤집기

--- 이코테/greedy/part3_03.py
+++ 이코테/greedy/part3_03.py
@@ -1,3 +1,19 @@
+#210421
+S = input()
+
+tmp1 = 0
+tmp2 = 0
+#0에서 1로 뒤집는 경우와 1에서 0으로 뒤집는 경우를 나눠서 체크
+for i in range(len(S)-1) :
+  if S[i] == '0' and S[i+1] == '1' :
+    tmp1 += 1
+  elif S[i] == '1' and S[i+1] == '0' :
+    tmp2 += 1
+
+#둘 중 한 경우가 0이 아니면 최소값을 출력, 하나라도 0이 나오면 최대값을 출력
+print(min(tmp1, tmp2) if not tmp1 and not tmp2 else max(tmp1, tmp2))
+
+#201111
 s = input()
 zCnt = s.count('0')
 oCnt = s.count('1')
